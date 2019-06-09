c4cc6b92a5c76318f9eb8204fc1da9ae061a4178
ibsd <icebergbsd@gmail.com>
2019-06-09 15:07:40 +0800

    twoSum: with loop once in 7ms

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -5,20 +5,27 @@ class TwoSum {
         HashMap<Integer, Integer> map = new HashMap<>();
         int index = 0;
         for (int num : nums) {
+
+            // case: {3, 3} 6
             if (map.containsKey(num)) {
                 if (target - num == num) {
                     return new int[]{map.get(num), index};
+                } else {
+                    index++;
                 }
-            }
-            map.put(num, index++);
-        }
-        for (int num : nums) {
-            int another = target - num;
-            if (another == num) {
-                continue;
-            }
-            if (map.containsKey(another)) {
-                return new int[]{map.get(num), map.get(another)};
+            } else {
+                map.put(num, index++);
+                int another = target - num;
+
+                // case: {3, 2, 4} 6
+                if (another == num) {
+                    continue;
+                }
+
+                // case: normal
+                if (map.containsKey(another)) {
+                    return new int[]{map.get(another), map.get(num)};
+                }
             }
         }
         return null;
