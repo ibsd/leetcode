6eb437ebaff3cd893b17ce1478acccdde26198e0
ibsd <icebergbsd@gmail.com>
2019-06-30 10:26:48 +0800

    findMedianSortedArrays with harf sort

--- src/main/java/FindMedianSortedArrays.java
+++ src/main/java/FindMedianSortedArrays.java
@@ -1,23 +1,26 @@
 public class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] result = mergeSort(nums1, nums2);
-        if (result.length % 2 == 1) {
-            return result[result.length / 2];
+        int top = (nums1.length + nums2.length) / 2 + 1;
+        int[] result = mergeSort(nums1, nums2, top);
+        if ((nums1.length + nums2.length) % 2 == 1) {
+            return result[result.length - 1];
         } else {
-            return (double)(result[result.length / 2] + result[result.length / 2 - 1]) / 2;
+            return (double) (result[result.length - 2] + result[result.length - 1]) / 2;
         }
     }
 
-    public int[] mergeSort(int[] nums1, int[] nums2) {
+    public int[] mergeSort(int[] nums1, int[] nums2, int top) {
         // define vars
-        int length = nums1.length + nums2.length;
-        int[] result = new int[length];
+        int[] result = new int[top];
         int index1 = 0;
         int index2 = 0;
         int indexResult = 0;
 
         // define loop cond
         while (index1 < nums1.length && index2 < nums2.length) {
+            if (indexResult >= top) {
+                break;
+            }
             if (nums1[index1] < nums2[index2]) {
                 result[indexResult++] = nums1[index1++];
             } else {
@@ -25,9 +28,15 @@ public class FindMedianSortedArrays {
             }
         }
         while (index1 < nums1.length) {
+            if (indexResult >= top) {
+                break;
+            }
             result[indexResult++] = nums1[index1++];
         }
         while (index2 < nums2.length) {
+            if (indexResult >= top) {
+                break;
+            }
             result[indexResult++] = nums2[index2++];
         }
         return result;
--- src/test/java/FindMedianSortedArraysTest.java
+++ src/test/java/FindMedianSortedArraysTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FindMedianSortedArraysTest {
     private FindMedianSortedArrays solution;
@@ -16,7 +17,7 @@ class FindMedianSortedArraysTest {
         int[] nums1 = {1, 2};
         int[] nums2 = {3, 4};
         int[] expected = {1, 2, 3, 4};
-        int[] actual = this.solution.mergeSort(nums1, nums2);
+        int[] actual = this.solution.mergeSort(nums1, nums2, nums1.length + nums2.length);
         assertArrayEquals(expected, actual);
     }
 
@@ -25,7 +26,7 @@ class FindMedianSortedArraysTest {
         int[] nums1 = {1, 3};
         int[] nums2 = {2};
         int[] expected = {1, 2, 3};
-        int[] actual = this.solution.mergeSort(nums1, nums2);
+        int[] actual = this.solution.mergeSort(nums1, nums2, nums1.length + nums2.length);
         assertArrayEquals(expected, actual);
     }
 
@@ -43,6 +44,24 @@ class FindMedianSortedArraysTest {
         int[] nums1 = {1, 3};
         int[] nums2 = {2};
         double expected = 2;
+        double actual = this.solution.findMedianSortedArrays(nums1, nums2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_find_median_two_sorted_arrays_one_empty_and_another_one() {
+        int[] nums1 = new int[0];
+        int[] nums2 = {1};
+        double expected = 1;
+        double actual = this.solution.findMedianSortedArrays(nums1, nums2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_find_median_two_sorted_arrays_one_empty_and_another_two() {
+        int[] nums1 = new int[0];
+        int[] nums2 = {2, 3};
+        double expected = 2.5;
         double actual = this.solution.findMedianSortedArrays(nums1, nums2);
         assertEquals(expected, actual);
     }
